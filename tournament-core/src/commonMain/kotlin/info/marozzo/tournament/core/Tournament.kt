e8773bbcf666a2e7c1074e0cafe3a62218e60068
Jakob Ecker <jakob.ecker42@gmail.com>
2023-05-31 23:36:13 +0200

    fully rework domain model

--- tournament-core/src/commonMain/kotlin/info/marozzo/tournament/core/Tournament.kt
+++ tournament-core/src/commonMain/kotlin/info/marozzo/tournament/core/Tournament.kt
@@ -1,13 +1,43 @@
 package info.marozzo.tournament.core
 
-import kotlinx.collections.immutable.ImmutableList
+/**
+ * A [Match] that should be played.
+ *
+ * This does not imply home / away positions of [Competitors][Competitor] but is an unordered pair.
+ * This means `([a], [b]) == ([b], [a])`.
+ */
+data class Match(val a: Competitor, val b: Competitor) {
+
+    /**
+     * A [Match] is equal if the [Competitors][Competitor] are the same,
+     * so ([a], [b]) == ([b], [a]).
+     */
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        }
+
+        if (other is Match) {
+            return (a == other.a && b == other.b) || (a == other.b && b == other.a)
+        }
+
+        return false
+    }
+
+    /**
+     * A [hashCode]-implementation that fulfills the contract ([a], [b]) == ([b], [a]).
+     */
+    override fun hashCode(): Int = 31 + (a.hashCode() * b.hashCode())
+}
 
 /**
- *  A [Tournament], played as part of an [Event].
+ * A [Competitor] of a [Match].
  *
- *  The [Tournament.participants] should be a subset of the [Event.participants].
+ * Can be [Fixed], i.e. known from the start or depend on the outcome of a previous [Match]
+ * ([WinnerOf], [LoserOf]).
  */
-data class Tournament(
-    val name: String,
-    val participants: ImmutableList<Participant>,
-)
+sealed class Competitor {
+    data class Fixed(val name: String): Competitor()
+    data class WinnerOf(val match: Match): Competitor()
+    data class LoserOf(val match: Match): Competitor()
+}
