fd4fa03db1e8591cf04f26c0a77f284a7f0a08c7
Jakob Ecker <jakob.ecker42@gmail.com>
2023-06-06 18:42:49 +0200

    make window open maximized

--- tournament-desktop/src/jvmMain/kotlin/info/marozzo/tournament/desktop/Main.kt
+++ tournament-desktop/src/jvmMain/kotlin/info/marozzo/tournament/desktop/Main.kt
@@ -1,15 +1,24 @@
 package info.marozzo.tournament.desktop
 
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import info.marozzo.tournament.desktop.components.App
 
 
 fun main() = application {
+    val windowState = rememberWindowState(
+        placement = WindowPlacement.Maximized, size = DpSize.Unspecified
+    )
+
     Window(
         title = "Tournament Planner",
-        onCloseRequest = ::exitApplication
+        state = windowState,
+        onCloseRequest = ::exitApplication,
     ) {
+
         App()
     }
 }
