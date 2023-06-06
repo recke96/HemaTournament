22ad74e8c852b616063ca7ba5b87475ddcb36597
Jakob Ecker <jakob.ecker42@gmail.com>
2023-06-06 18:57:00 +0200

    add close confirmation dialog

--- tournament-desktop/src/jvmMain/kotlin/info/marozzo/tournament/desktop/Main.kt
+++ tournament-desktop/src/jvmMain/kotlin/info/marozzo/tournament/desktop/Main.kt
@@ -1,5 +1,16 @@
 package info.marozzo.tournament.desktop
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
@@ -8,17 +19,48 @@ import androidx.compose.ui.window.rememberWindowState
 import info.marozzo.tournament.desktop.components.App
 
 
+@OptIn(ExperimentalMaterialApi::class)
 fun main() = application {
     val windowState = rememberWindowState(
         placement = WindowPlacement.Maximized, size = DpSize.Unspecified
     )
+    val (isCloseRequested, setIsCloseRequested) = remember { mutableStateOf(false) }
 
     Window(
         title = "Tournament Planner",
         state = windowState,
-        onCloseRequest = ::exitApplication,
+        onCloseRequest = { setIsCloseRequested(true) },
     ) {
 
         App()
+
+        AnimatedVisibility(visible = isCloseRequested) {
+            val focusRequester = remember { FocusRequester() }
+            AlertDialog(
+                onDismissRequest = { /* Ignore since the user should explicitly cancel or confirm */ },
+                title = { Text("Close Tournament Planner?") },
+                text = { Text("Are you sure you want to close the application?") },
+                confirmButton = {
+                    Button(
+                        onClick = { this@application.exitApplication() },
+                        modifier = Modifier.focusRequester(focusRequester)
+                    ) {
+                        Icon(Icons.Default.Check, contentDescription = null)
+                        Text("Ok")
+                    }
+                },
+                dismissButton = {
+                    OutlinedButton(onClick = { setIsCloseRequested(false) }) {
+                        Icon(Icons.Default.Close, contentDescription = null)
+                        Text("Cancel")
+                    }
+                }
+            )
+
+            LaunchedEffect(Unit) {
+                focusRequester.requestFocus()
+            }
+        }
+
     }
 }
