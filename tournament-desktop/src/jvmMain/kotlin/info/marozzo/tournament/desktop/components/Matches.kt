896f33d7c77998d7663c28741fbc493c899d66a9
Jakob Ecker <jakob.ecker42@gmail.com>
2023-06-09 14:13:42 +0200

    rework matches component to display the rounds correctly

--- tournament-desktop/src/jvmMain/kotlin/info/marozzo/tournament/desktop/components/Matches.kt
+++ tournament-desktop/src/jvmMain/kotlin/info/marozzo/tournament/desktop/components/Matches.kt
@@ -1,10 +1,11 @@
 package info.marozzo.tournament.desktop.components
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -12,31 +13,31 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import info.marozzo.tournament.core.Competitor
-import info.marozzo.tournament.core.Match
 import info.marozzo.tournament.core.Participant
+import info.marozzo.tournament.core.Round
 import info.marozzo.tournament.core.matchgenerators.MatchGenerator
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
 @Composable
 fun Matches(
     generator: MatchGenerator,
     participants: ImmutableList<Participant>,
     modifier: Modifier = Modifier,
-) = Surface(modifier = modifier) {
+) = Surface(modifier = modifier, color = MaterialTheme.colors.primarySurface) {
     val (isLoading, setIsLoading) = remember { mutableStateOf(false) }
-    val (matches, setMatches) = remember { mutableStateOf<ImmutableList<Match>>(persistentListOf()) }
+    val (rounds, setRounds) = remember { mutableStateOf<ImmutableList<Round>>(persistentListOf()) }
 
-    fun Competitor.text(): String = when (this) {
+    fun Competitor.text(rounds: List<Round>): String = when (this) {
         is Competitor.Fixed -> this.participant.name
-        is Competitor.WinnerOf -> "Winner of Match ${matches.indexOf(this.match) + 1}"
-        is Competitor.LoserOf -> "Loser of Match ${matches.indexOf(this.match) + 1}"
+        is Competitor.WinnerOf -> "Winner of Round ${rounds.find { it.matches.contains(match) }?.rank}, Match ${match.rank}"
+        is Competitor.LoserOf -> "Loser of Round ${rounds.find { it.matches.contains(match) }?.rank}, Match ${match.rank}"
     }
 
     LaunchedEffect(generator, participants) {
         setIsLoading(true)
-        setMatches(generator.generate(participants))
+        setRounds(generator.generate(participants))
         setIsLoading(false)
     }
 
@@ -45,11 +46,15 @@ fun Matches(
             LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
         }
         LazyColumn(modifier = Modifier.fillMaxWidth()) {
-            itemsIndexed(matches) { idx, match ->
-                ListItem {
-                    Text("${idx + 1}. ${match.a.text()} vs. ${match.b.text()}")
+            rounds.forEach { round ->
+                stickyHeader(key = "r${round.rank}", contentType = "header") {
+                    ListItem { Text("Round ${round.rank}") }
                 }
-                Divider()
+                items(round.matches, key = { "r${round.rank}m${it.rank}" }, contentType = { "match" }) {
+                    ListItem {
+                        Text("${it.rank}. ${it.a.text(rounds)} vs. ${it.b.text(rounds)}")
+                    }
+                }
             }
         }
     }
