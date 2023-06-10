03cf47debfbc6ad31a49660b80d094853e3bb3db
Jakob Ecker <jakob.ecker42@gmail.com>
2023-06-10 22:14:16 +0200

    cleanup

--- tournament-desktop/src/jvmMain/kotlin/info/marozzo/tournament/desktop/components/Matches.kt
+++ tournament-desktop/src/jvmMain/kotlin/info/marozzo/tournament/desktop/components/Matches.kt
@@ -6,13 +6,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.ListItem
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import info.marozzo.tournament.core.Competitor
 import info.marozzo.tournament.core.Participant
 import info.marozzo.tournament.core.Round
