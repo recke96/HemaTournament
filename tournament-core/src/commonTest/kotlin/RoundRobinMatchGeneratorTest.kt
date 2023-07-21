bfd6f790e3b7a76c839685d52e56532010d43d3a
Jakob Ecker <jakob.ecker42@gmail.com>
2023-07-21 13:46:24 +0200

    property test round-robin match gen

--- tournament-core/src/commonTest/kotlin/RoundRobinMatchGeneratorTest.kt
+++ tournament-core/src/commonTest/kotlin/RoundRobinMatchGeneratorTest.kt
@@ -1,24 +1,23 @@
+import info.marozzo.tournament.core.Competitor
 import info.marozzo.tournament.core.matchgenerators.RoundRobinTournamentMatchGenerator
 import io.kotest.assertions.assertSoftly
+import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.inspectors.forAll
-import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.*
 import io.kotest.matchers.shouldBe
 import io.kotest.property.Arb
-import io.kotest.property.PropertyTesting
-import io.kotest.property.arbitrary.list
 import io.kotest.property.arbitrary.map
+import io.kotest.property.arbitrary.set
 import io.kotest.property.checkAll
 import kotlinx.collections.immutable.toImmutableList
 
 class RoundRobinMatchGeneratorTest : FunSpec({
 
-    PropertyTesting
-
     val sut = RoundRobinTournamentMatchGenerator()
 
     context("No Matches tournaments") {
-        checkAll(Arb.list(Arb.participant(), 0..1).map { it.toImmutableList() }) { participants ->
+        checkAll(Arb.set(Arb.participant(), 0..1).map { it.toImmutableList() }) { participants ->
             collect("number_participants", participants.size)
 
             sut.generate(participants.toImmutableList()).shouldBeEmpty()
@@ -26,18 +25,40 @@ class RoundRobinMatchGeneratorTest : FunSpec({
     }
 
     context("Tournaments") {
-        checkAll(Arb.list(Arb.participant(), 2..1000).map{ it.toImmutableList() }) { participants ->
-            collect("number_participants", participants.size)
+        checkAll(Arb.set(Arb.participant(), 2..100).map { it.toImmutableList() }) { participants ->
+            val n = participants.size
+            collect("number_participants", n)
 
-            val expectedNumberOfRounds = if (participants.size % 2 == 0) participants.size - 1 else participants.size
-            val expectedTotalNumberOfMatches = (participants.size * (participants.size - 1)) / 2
-            val expectedNumberOfMatchesPerRound = participants.size / 2
+            val expectedNumberOfRounds = if (n % 2 == 0) n - 1 else n
+            val expectedTotalNumberOfMatches = (n * (n - 1)) / 2
+            val expectedNumberOfMatchesPerRound = n / 2
             val result = sut.generate(participants)
 
             assertSoftly(result) {
-                size shouldBe expectedNumberOfRounds
-                forAll { it.matches.size shouldBe expectedNumberOfMatchesPerRound }
-                sumOf { it.matches.size } shouldBe expectedTotalNumberOfMatches
+                withClue({ "Expect n - 1 rounds if n is even and n rounds if n is odd (n=$n)" }) {
+                    size shouldBe expectedNumberOfRounds
+                }
+                withClue({ "Expect (n * (n - 1))/2 number of matches (n=$n)" }) {
+                    sumOf { it.matches.size } shouldBe expectedTotalNumberOfMatches
+                }
+                forAll { round ->
+                    assertSoftly(round.matches) {
+                        withClue({ "Expect (n - 1)/2 matches if n is even and n/2 rounds if n is odd (n=$n)" }) {
+                            size shouldBe expectedNumberOfMatchesPerRound
+                        }
+                        val roundParticipants = flatMap { sequenceOf(it.a, it.b) }
+                            .map { it as Competitor.Fixed }
+                            .map { it.participant }
+
+                        withClue({ "Every participant, except maybe one, should have a match in round ${round.rank}" }) {
+                            roundParticipants
+                                .shouldHaveAtLeastSize(n - 1)
+                                .shouldHaveAtMostSize(n)
+                                .shouldBeUnique()
+                                .forAll { it.shouldBeIn(participants) }
+                        }
+                    }
+                }
             }
         }
     }
