eeaef87496654cde91d94c795c21d5fadf952b41
Jakob Ecker <jakob.ecker42@gmail.com>
2023-09-03 13:11:57 +0200

    make state immutable

--- tournament-desktop/src/jvmMain/kotlin/info/marozzo/tournament/desktop/application/stores/tournament/TournamentState.kt
+++ tournament-desktop/src/jvmMain/kotlin/info/marozzo/tournament/desktop/application/stores/tournament/TournamentState.kt
@@ -1,5 +1,8 @@
 package info.marozzo.tournament.desktop.application.stores.tournament
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 internal sealed class TournamentState
 
 internal data object NoEventState : TournamentState();
