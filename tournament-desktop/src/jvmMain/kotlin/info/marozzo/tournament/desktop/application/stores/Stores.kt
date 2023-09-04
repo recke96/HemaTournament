3a7b3b350bc0b6ce8ace490adec67e7dbb22cfd0
Jakob Ecker <jakob.ecker42@gmail.com>
2023-09-04 23:35:38 +0200

    remove koin

--- tournament-desktop/src/jvmMain/kotlin/info/marozzo/tournament/desktop/application/stores/Stores.kt
+++ tournament-desktop/src/jvmMain/kotlin/info/marozzo/tournament/desktop/application/stores/Stores.kt
@@ -1,11 +1,32 @@
 package info.marozzo.tournament.desktop.application.stores
 
 import arrow.fx.coroutines.ResourceScope
-import com.arkivanov.mvikotlin.core.store.StoreFactory
+import com.arkivanov.mvikotlin.core.utils.setMainThreadId
 import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
+import info.marozzo.tournament.desktop.application.stores.tournament.TournamentExecutor
+import info.marozzo.tournament.desktop.application.stores.tournament.TournamentReducer
+import info.marozzo.tournament.desktop.application.stores.tournament.TournamentStore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.swing.Swing
+import kotlinx.coroutines.withContext
+
+
+internal interface StoresContext {
+    val tournament: TournamentStore
+}
 
 context (ResourceScope)
-internal suspend fun storeFactory(): StoreFactory = install(
-    { DefaultStoreFactory() },
-    { _, _ -> }
-)
+internal suspend fun <T> withStores(block: suspend StoresContext.() -> T): T {
+    withContext(Dispatchers.Swing) {
+        setMainThreadId(Thread.currentThread().id)
+    }
+
+    val factory = DefaultStoreFactory()
+    val tournament = TournamentStore.create(factory, ::TournamentExecutor, TournamentReducer)
+
+    val context = object : StoresContext {
+        override val tournament = tournament
+    }
+
+    return context.block()
+}
